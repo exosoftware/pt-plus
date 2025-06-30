40aba2f12a84a682dd3a30ea5695e0e46c9a1d87
pedrocs-exo <pedrocs@exo.pt>
2025-07-01 00:26:22 +0100

    PT+ bot

---
+++ ptplus_pos/migrations/14.0.4.2.0/pre-migration.py
@@ -0,0 +1,19 @@
+# Copyright 2021 Tecnativa - João Marques
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from openupgradelib import openupgrade
+from psycopg2 import sql
+
+
+@openupgrade.migrate()
+def migrate(env, version):
+    openupgrade.logged_query(
+        env.cr,
+        sql.SQL(
+            """
+            UPDATE pos_order
+            SET pos_reference = ptplus_unique_id,
+            ptplus_unique_id = pos_reference
+        """
+        ),
+    )
